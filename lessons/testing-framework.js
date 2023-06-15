7554845b5579d92e2a739b74005e1613af6c1a0a
jinwoo-lee <jw.lee@nhn.com>
2023-06-15 22:27:00 +0900

    04. 테스트 캡슐화시키기

--- lessons/testing-framework.js
+++ lessons/testing-framework.js
@@ -1,31 +1,16 @@
 const {sum, subtract} = require('../math')
 
-function testSum() {
+test('sum adds numbers', () => {
   const result = sum(3, 7)
   const expected = 10
   expect(result).toBe(expected)
-}
+})
 
-function testSubtract() {
+test('subtract subtracts numbers', () => {
   const result = subtract(7, 3)
   const expected = 4
   expect(result).toBe(expected)
-}
-
-test('sum add numbers', testSum);
-
-test('subtract subtracts numbers', testSubtract);
-// test('sum adds numbers', () => {
-//   const result = sum(3, 7)
-//   const expected = 10
-//   expect(result).toBe(expected)
-// })
-
-// test('subtract subtracts numbers', () => {
-//   const result = subtract(7, 3)
-//   const expected = 4
-//   expect(result).toBe(expected)
-// })
+})
 
 function test(title, callback) {
   try {
